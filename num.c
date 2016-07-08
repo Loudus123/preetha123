aafc906c2371570f0147fb6788e3425dc9b778d8
Loudus123 <loudusraj@gmail.com>
2016-07-08 14:31:13 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i=0,n;
+while(i==0)
+{
+if(n>0)
+printf("number is positive");
+elseif(n<0)
+printf("number is negative");
+else
+printf("the number is nutral");
+}
