5f0e820e43651343bda4a7ce505974bbb62f936d
Loudus123 <loudusraj@gmail.com>
2016-07-08 14:23:01 +0530

    finish

---
+++ number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i=0;
+while(i==0)
+{
+if(n>0)
+printf("number is positive");
+else(n<0)
+printf("the number is negative");
+}
+printf("the number is 0");
+getch();
+}
