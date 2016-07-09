b652d7c5f9d5907a1340536fd051c64d64aa00cc
Loudus123 <loudusraj@gmail.com>
2016-07-09 10:58:33 +0530

    Create num1.c

---
+++ num1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n;
+if(n!=0)
+{
+if(n<0)
+printf("the number is negative");
+else
+printf("the number is positive");
+}
+else
+printf("the number is zero");
+}
